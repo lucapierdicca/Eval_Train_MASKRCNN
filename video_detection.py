a806e1894d4ac35a53fb8cae98b2a5070d6a769f
lucapierdicca <luca.pierdicca@gmail.com>
2018-06-04 22:19:20 +0200

    adjusted path

--- video_detection.py
+++ video_detection.py
@@ -83,7 +83,7 @@ def display_instances(image, boxes, masks, class_ids, class_names,
 
     masked_image = image.astype(np.uint32).copy()
     for i in range(N):
-        color = colors[i]
+        color = colors[class_ids[i]]
 
         # Bounding box
         if not np.any(boxes[i]):
