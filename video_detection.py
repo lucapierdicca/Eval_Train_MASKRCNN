c9a0db71eada183cff77a41d7b35a476fa40d8a2
lucapierdicca <luca.pierdicca@gmail.com>
2018-06-04 19:50:37 +0200

    adjusted path

--- video_detection.py
+++ video_detection.py
@@ -69,8 +69,96 @@ def color_splash(image, mask):
     return splash
 
 
+def display_instances(image, boxes, masks, class_ids, class_names,
+                      scores=None, title="",
+                      figsize=(16, 16), ax=None,
+                      show_mask=True, show_bbox=True,
+                      colors=None, captions=None):
+    """
+    boxes: [num_instance, (y1, x1, y2, x2, class_id)] in image coordinates.
+    masks: [height, width, num_instances]
+    class_ids: [num_instances]
+    class_names: list of class names of the dataset
+    scores: (optional) confidence scores for each box
+    title: (optional) Figure title
+    show_mask, show_bbox: To show masks and bounding boxes or not
+    figsize: (optional) the size of the image
+    colors: (optional) An array or colors to use with each object
+    captions: (optional) A list of strings to use as captions for each object
+    """
+    # Number of instances
+    N = boxes.shape[0]
+    if not N:
+        print("\n*** No instances to display *** \n")
+    else:
+        assert boxes.shape[0] == masks.shape[-1] == class_ids.shape[0]
 
-def detect_and_color_splash(model, dataset, video_path=None):
+    # If no axis is passed, create one and automatically call show()
+    auto_show = False
+    if not ax:
+        _, ax = plt.subplots(1, figsize=figsize)
+        auto_show = False
+
+    # Generate random colors
+    colors = colors or random_colors(N)
+
+    # Show area outside image boundaries.
+    height, width = image.shape[:2]
+    ax.set_ylim(height + 10, -10)
+    ax.set_xlim(-10, width + 10)
+    ax.axis('off')
+    ax.set_title(title)
+
+    masked_image = image.astype(np.uint32).copy()
+    for i in range(N):
+        color = colors[i]
+
+        # Bounding box
+        if not np.any(boxes[i]):
+            # Skip this instance. Has no bbox. Likely lost in image cropping.
+            continue
+        y1, x1, y2, x2 = boxes[i]
+        if show_bbox:
+            p = patches.Rectangle((x1, y1), x2 - x1, y2 - y1, linewidth=2,
+                                alpha=0.7, linestyle="dashed",
+                                edgecolor=color, facecolor='none')
+            ax.add_patch(p)
+
+        # Label
+        if not captions:
+            class_id = class_ids[i]
+            score = scores[i] if scores is not None else None
+            label = class_names[class_id]
+            x = random.randint(x1, (x1 + x2) // 2)
+            caption = "{} {:.3f}".format(label, score) if score else label
+        else:
+            caption = captions[i]
+        ax.text(x1, y1 + 8, caption,
+                color='w', size=11, backgroundcolor="none")
+
+        # Mask
+        mask = masks[:, :, i]
+        if show_mask:
+            masked_image = apply_mask(masked_image, mask, color)
+
+        # Mask Polygon
+        # Pad to ensure proper polygons for masks that touch image edges.
+        padded_mask = np.zeros(
+            (mask.shape[0] + 2, mask.shape[1] + 2), dtype=np.uint8)
+        padded_mask[1:-1, 1:-1] = mask
+        contours = find_contours(padded_mask, 0.5)
+        for verts in contours:
+            # Subtract the padding and flip (y, x) to (x, y)
+            verts = np.fliplr(verts) - 1
+            p = Polygon(verts, facecolor="none", edgecolor=color)
+            ax.add_patch(p)
+    ax.imshow(masked_image.astype(np.uint8))
+    if auto_show:
+        plt.show()
+
+
+
+def detection_to_video(model, dataset, video_path=None):
         
     import cv2
     # Video capture
@@ -79,57 +167,62 @@ def detect_and_color_splash(model, dataset, video_path=None):
     height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
     fps = vcapture.get(cv2.CAP_PROP_FPS)
     n_frames = int(vcapture.get(cv2.CAP_PROP_FRAME_COUNT))
-
     print("Tot frames: %d" % n_frames)
 
     
-    # Define codec and create video writer
-    file_name = "{:%Y%m%dT%H%M%S}.mp4".format(datetime.datetime.now())
-    detection_name = "{:%Y%m%dT%H%M%S}.pickle".format(datetime.datetime.now())
-    vwriter = cv2.VideoWriter(file_name, cv2.VideoWriter_fourcc(*'MJPG'), fps, (width, height))
-    print(fps)
+    detection_list_name = "det_list_"+video_path
+    
     count = 0
     success = True
     detection_list = []
+    early_stop = 100
 	
     while success:
         print("frame: %d / %d" % (count, n_frames))
         # Read next image
         success, image = vcapture.read()
-        print(success)
         if success:
             # OpenCV returns images as BGR, convert to RGB
-            print(image.shape)
             image = image[..., ::-1]
-            print(image.shape)
             # Detect objects
             r = model.detect([image], verbose=0)[0]
             print("ROIS: %d" % len(r['rois']))
             detection_list.append(r)
 			# Create a plot made of frame+masks+bboxes
-            visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'], dataset.class_names, scores=r['scores'], ax = get_ax(1))
+            display_instances(image, r['rois'], r['masks'], r['class_ids'], dataset.class_names, scores=r['scores'])
             # Save it on the HDD
-            #plt.figure(figsize=(width,height))
             plt.savefig("image" + str(count),facecolor=None, edgecolor=None)
-            plt.clf()
             plt.close()
-            # Load it back as a numpy array
-            numpy_frame = cv2.imread("image" + str(count) + '.png')
-            #numpy_frame = numpy_frame.astype(np.uint8)
-            print(numpy_frame.shape)
-            # RGB -> BGR to save image to video
-            #numpy_frame = numpy_frame[..., ::-1]
-            print(numpy_frame.shape)
-            # Add image to video writer
-            vwriter.write(numpy_frame)
+            
             count += 1
-            if count == 100:
+
+            if count == early_stop:
                 break
           
+    pickle.dump(detection_list, open(detection_list_name, 'wb'))
+
+
+
+    img_range = 0
+    if early_stop == 0:
+        img_range=n_frames
+    else:
+        img_range = early_stop
+
+    file_name = "detected_"+video_path
+    
+    img = cv2.imread('image0.png')
+    height,width,layers=img.shape
+    
+    vwriter = cv2.VideoWriter(file_name, cv2.VideoWriter_fourcc(*'MJPG'), fps, (width, height))
+    
+    for i in range(img_range-1):
+        img = cv2.imread('image'+str(i)+'.png')
+        vwriter.write(img)
+
     vwriter.release()
     print("Saved to ", file_name)
-    pickle.dump(detection_list, open(detection_name, 'wb'))
-    
+
 
     
 
