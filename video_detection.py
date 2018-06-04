496aeb957156148eff9caf84f9f1fab8eadecff3
lucapierdicca <luca.pierdicca@gmail.com>
2018-06-04 15:07:01 +0200

    added video_detection

---
+++ video_detection.py
@@ -0,0 +1,194 @@
+import os
+import sys
+import random
+import math
+import re
+import time
+import numpy as np
+import tensorflow as tf
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+
+from mrcnn import utils
+from mrcnn import visualize
+from mrcnn.visualize import display_instances
+import mrcnn.model as modellib
+from mrcnn.model import log
+
+import visiope_full
+import cv2
+
+
+import datetime
+import time
+import skimage.draw
+from skimage import io
+import pickle
+
+
+
+def get_ax(rows=1, cols=1, size=16):
+    """Return a Matplotlib Axes array to be used in
+    all visualizations in the notebook. Provide a
+    central point to control graph sizes.
+    
+    Adjust the size attribute to control how big to render images
+    """
+    _, ax = plt.subplots(rows, cols, figsize=(size*cols, size*rows))
+    
+    return ax
+
+	
+	
+	
+def color_splash(image, mask):
+    """Apply color splash effect.
+    image: RGB image [height, width, 3]
+    mask: instance segmentation mask [height, width, instance count]
+
+    Returns result image.
+    """
+    # Make a grayscale copy of the image. The grayscale copy still
+    # has 3 RGB channels, though.
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    
+    #print(type(gray))
+    # We're treating all instances as one, so collapse the mask into one layer
+    mask = (np.sum(mask, -1, keepdims=True) >= 1)
+    #print(mask.shape)
+    # Copy color pixels from the original color image where mask is set
+    
+    
+    if mask.shape[0] == gray.shape[0]:
+        splash = np.where(mask, image, gray).astype(np.uint8)
+    else:
+        mask = np.full((gray.shape[0], gray.shape[1], gray.shape[2]), False)
+        
+        #print(mask)
+        splash = np.where(mask, image, gray).astype(np.uint8)
+            
+        
+    return splash
+	
+def detect_and_color_splash(model, dataset, image_path=None, video_path=None):
+    assert image_path or video_path
+
+        
+    import cv2
+    # Video capture
+    vcapture = cv2.VideoCapture(video_path)
+    width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps = vcapture.get(cv2.CAP_PROP_FPS)
+	n_frames = vcapture.get(cv2.CAP_PROP_FRAME_COUNT)
+	print("Tot frames: %d" % n_frames)
+    
+    # Define codec and create video writer
+    file_name = "{:%Y%m%dT%H%M%S}.mp4".format(datetime.datetime.now())
+	detection_name = "{:%Y%m%dT%H%M%S}.pickle".format(datetime.datetime.now())
+    vwriter = cv2.VideoWriter(file_name,
+                              cv2.VideoWriter_fourcc(*'MJPG'),
+                              fps, (width, height))
+    
+    count = 0
+    success = True
+    detection_list = []
+	
+    while success:
+        print("frame: %d / %d" % (count, n_frames))
+        # Read next image
+        success, image = vcapture.read()
+        print(success)
+        if success:
+            # OpenCV returns images as BGR, convert to RGB
+            image = image[..., ::-1]
+            # Detect objects
+            r = model.detect([image], verbose=0)[0]
+			print("ROIS: %d" % len(r['rois']))
+			detection_list.append(r)
+			# Create a plot made of frame+masks+bboxes
+            visualize.display_instances(image, r['rois'], r['masks'], r['scores'], dataset.class_names, ax = get_ax(1))
+            # Save it on the HDD
+			plt.savefig("image" + str(count))
+            # Load it back as a numpy array
+            numpy_frame = io.imread("image" + str(count))
+            # RGB -> BGR to save image to video
+            numpy_frame = numpy_frame[..., ::-1]
+            # Add image to video writer
+            vwriter.write(numpy_frame)
+            count += 1
+          
+    vwriter.release()
+    print("Saved to ", file_name)
+	pickle.dump(detection_list, open(detection_name, 'wb'))
+    
+
+    
+
+# CONFIG
+#--------------------------------------------------------------------
+config = visiope_full.VisiopeConfig()
+
+# Override the training configurations with a few
+# changes for inferencing.
+class InferenceConfig(config.__class__):
+    # Run detection on one image at a time
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+    #DETECTION_MIN_CONFIDENCE = 0
+
+config = InferenceConfig()
+#config.display()
+DEVICE = "/gpu:0"  # /cpu:0 or /gpu:0 
+
+
+# MODEL
+#---------------------------------------------------------------------
+# Create model in inference mode
+MODEL_DIR= r"C:\Users\Edoardo\My_Works\FILES PER PROGETTO"
+
+with tf.device(DEVICE):
+    model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR,
+                              config=config)
+
+
+# LOAD CHKPT
+#----------------------------------------------------------------------------------
+weights_path = r"C:\Users\Edoardo\My_Works\FILES PER PROGETTO\mask_rcnn_visiope_0080.h5"
+print("Model weights path: ", weights_path)
+model.load_weights(weights_path, by_name=True)
+
+
+
+# LOAD DATASET (we need it to obtain the mapping class_id:class_name)
+#---------------------------------------------------------------------------------
+print("\n======VALIDATION SET======")
+dataset_val = visiope_full.VisiopeDataset()
+
+dataset_val.load_visiope(sampling="val")
+print("\nN tot_visiope images: %d" % len(dataset_val.b))
+
+n_val_visiope_imgs = len(dataset_val.image_info)
+print("N val_visiope images: %d" % n_val_visiope_imgs)
+
+
+dataset_val.load_coco(sampling="val", class_ids=selected_COCO_class_ids)
+n_val_COCO_imgs = len(dataset_val.image_info)-n_val_visiope_imgs
+print("N val_COCO images: %d" % n_val_COCO_imgs)
+
+dataset_val.prepare()
+print("N tot val images (val_visiope + val_COCO): %d\n" % len(dataset_val.image_info))
+
+
+
+# VIDEO
+#---------------------------------------------------------------------
+video_path= r'E:\Edoardo\Università\Magistrale\Vision and Perception\V&P - video for project\Personal Care\Shaving\DLJFoRluYx4.mp4'
+#cap = cv2.VideoCapture(r'E:\Edoardo\Università\Magistrale\Vision and Perception\V&P - video for project\Personal Care\Shaving\DLJFoRluYx4.mp4')
+
+
+
+# START VIDEO CREATION
+detect_and_color_splash(model, dataset_val video_path)
+
