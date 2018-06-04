e42b184ae85ce973b4c7fa91ab8e3fd404e641d8
lucapierdicca <luca.pierdicca@gmail.com>
2018-06-04 16:46:02 +0200

    adjusted path

--- video_detection.py
+++ video_detection.py
@@ -202,7 +202,7 @@ print("N tot val images (val_visiope + val_COCO): %d\n" % len(dataset_val.image_
 
 # VIDEO
 #---------------------------------------------------------------------
-video_path= r'v.mp4'
+video_path= 'v.mp4'
 
 detect_and_color_splash(model, dataset_val, video_path)
 
