bde349b8bf12124f2263316843f5819dfb242ba2
lucapierdicca <luca.pierdicca@gmail.com>
2018-06-20 18:14:52 +0200

    fixing

--- validation.py
+++ validation.py
@@ -311,7 +311,7 @@ classes = {'Electric Razor':1,
             'Wash Basin':15
             }
 
-for index,json_elem in enumerate(dataset_val.image_info):
+for index,json_elem in enumerate(dataset_val.image_info)[:1]:
     if b[json_elem['id']]['Label'] == 'Skip':
         continue
     # read image for sizes
@@ -329,6 +329,7 @@ for index,json_elem in enumerate(dataset_val.image_info):
 
     w, h = im.size
     seg = b[json_elem['id']]["Label"]
+    print(seg)
     maskMat = []
     idClassi = []
 
@@ -351,6 +352,8 @@ for index,json_elem in enumerate(dataset_val.image_info):
             for ind in range(len(x_coord)):
                 coord.append(x_coord[ind])
                 coord.append(y_coord[ind])
+            print(coord)
+            
             immagine = coordToMatrix(coord, w, h)
             centroidi_lista.append(find_centroid(immagine))
             aree.append(compute_area(immagine))
